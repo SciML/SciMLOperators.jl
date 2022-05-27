03577ca73a57c224644d673740e75666235fa13f
xtalax <alex.lemony@gmail.com>
2022-05-27 14:05:13 +0100

    second commit

--- src/SciMLOperators.jl
+++ src/SciMLOperators.jl
@@ -20,16 +20,30 @@ abstract type AbstractDiffEqLinearOperator{T} <: AbstractDiffEqOperator{T} end
 $(TYPEDEF)
 """
 abstract type AbstractDiffEqCompositeOperator{T} <: AbstractDiffEqLinearOperator{T} end
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractMatrixFreeOperator{T} <: AbstractDiffEqLinearOperator{T} end
 
 include("interface.jl")
 include("operators/basic_operators.jl")
-include("operators/diffeq_operator.jl")
 include("operators/common_defaults.jl")
+include("operators/diffeq_operator.jl")
+include("operators/matrixfree_operators.jl")
+include("operators/composite_operators.jl")
 
+# The (u,p,t) and (du,u,p,t) interface
+for T in [DiffEqScaledOperator, DiffEqOperatorCombination, DiffEqOperatorComposition]
+    (L::T)(u, p, t) = (update_coefficients!(L, u, p, t); L * u)
+    (L::T)(du, u, p, t) = (update_coefficients!(L, u, p, t); mul!(du, L, u))
+end
 
 export AffineDiffEqOperator, DiffEqScaledOperator
 
 export DiffEqScalar, DiffEqArrayOperator, DiffEqIdentity
+
+export MatrixFreeOperator
+
 
 export update_coefficients!, update_coefficients,
     has_adjoint, has_expmv!, has_expmv, has_exp, has_mul, has_mul!, has_ldiv, has_ldiv!
---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using SafeTestsets
+
+const GROUP = get(ENV, "GROUP", "All")
+const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
+const is_TRAVIS = haskey(ENV, "TRAVIS")
+
+@time begin
+    if GROUP == "All" || GROUP == "OperatorInterface"
+        @time @safetestset "Matrix-Free Operators" begin
+            include("operators/matrixfree.jl")
+        end
+        @time @safetestset "Composite Operators Interface" begin
+            include("operators/composite_operators_interface.jl")
+        end
+        @time @safetestset "DiffEqOperator tests" begin
+            include("operators/diffeqoperator.jl")
+        end
+
+    end
+end
