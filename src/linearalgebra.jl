b0159639af0ba8d512461b30a82b0cca2d5315c1
Vedant Puri <vedantpuri@gmail.com>
2022-06-04 12:59:58 -0400

    adjoint and transpose

--- src/linearalgebra.jl
+++ src/linearalgebra.jl
@@ -1,4 +1,7 @@
 #
+AbstractAdjointedVector  = Adjoint{  <:Number, <:AbstractVector}
+AbstractTransposedVector = Transpose{<:Number, <:AbstractVector}
+
 struct AdjointedOperator{T,LType} <: AbstractSciMLOperator{T}
     L::LType
 
@@ -7,53 +10,67 @@ struct AdjointedOperator{T,LType} <: AbstractSciMLOperator{T}
     end
 end
 
-Base.adjoint(L::AbstractSciMLOperator) = AdjointedOperator(L)
-Base.adjoint(L::AdjointedOperator) = L.L
+struct TransposedOperator{T,LType} <: AbstractSciMLOperator{T}
+    L::LType
 
-Base.size(L::AdjointedOperator) = size(L.L) |> reverse
-
-has_adjoint(L::AdjointedOperator) = true
-
-@forward AdjointedOperator.L (
-                              # Base
-                              convert,
-
-                              # LinearAlgebra
-                              LinearAlgebra.isreal,
-                              LinearAlgebra.issymmetric,
-                              LinearAlgebra.ishermitian,
-                              LinearAlgebra.isposdef,
-                              LinearAlgebra.opnorm,
-
-                              # SciML
-                              getops,
-
-                              isconstant,
-                              has_mul!,
-                              has_ldiv,
-                              has_ldiv!,
-                             )
-
-
-# oeprator application
-AbstractAdjointedVector = Adjoint{<:Number, <:AbstractVector}
-
-Base.:*(u::AbstractAdjointedVector, L::AdjointedOperator) = (L.L * u.parent)'
-Base.:/(u::AbstractAdjointedVector, L::AdjointedOperator) = (L.L \ u.parent)'
-
-function LinearAlgebra.mul!(v::AbstractAdjointedVector, L::AdjointedOperator, u::AbstractAdjointedVector)
-    mul!(v.parent, L.L, u.parent)'
+    function TransposedOperator(L::AbstractSciMLOperator{T}) where{T}
+        new{T,typeof(L)}(L)
+    end
 end
 
-function LinearAlgebra.mul!(v::AbstractAdjointedVector, L::AdjointedOperator, u::AbstractAdjointedVector, α::Number, β::Number)
-    mul!(v.parent, L.L, u.parent, α, β)'
-end
+for (op, LType, VType) in (
+                           (:adjoint,   :AdjointedOperator,  :AbstractAdjointedVector ),
+                           (:transpose, :TransposedOperator, :AbstractTransposedVector),
+                          )
+    # constructor
+    @eval Base.$op(L::AbstractSciMLOperator) = $LType(L)
 
-function LinearAlgebra.ldiv!(v::AbstractAdjointedVector, L::AdjointedOperator, u::AbstractAdjointedVector)
-    ldiv!(v.parent, L.L, u.parent)'
-end
+    @eval Base.convert(AbstractMatrix, L::$LType) = $op(convert(AbstractMatrix, L))'
 
-function LinearAlgebra.ldiv!(L::AdjointedOperator, u::AbstractAdjointedVector)
-    ldiv!(L.L, u.parent)'
+    # traits
+    @eval Base.size(L::$LType) = size(L.L) |> reverse
+    @eval Base.$op(L::$LType) = L.L
+
+    @eval has_adjoint(L::$LType) = true
+
+    @eval @forward $LType.L (
+                             # LinearAlgebra
+                             LinearAlgebra.isreal,
+                             LinearAlgebra.issymmetric,
+                             LinearAlgebra.ishermitian,
+                             LinearAlgebra.isposdef,
+                             LinearAlgebra.opnorm,
+    
+                             # SciML
+                             getops,
+    
+                             isconstant,
+                             has_mul!,
+                             has_ldiv,
+                             has_ldiv!,
+                            )
+
+    # oeprator application
+    @eval Base.:*(u::$VType, L::$LType) = $op(L.L * u.parent)
+    @eval Base.:/(u::$VType, L::$LType) = $op(L.L \ u.parent)
+
+    @eval function LinearAlgebra.mul!(v::$VType, L::$LType, u::$VType)
+        mul!(v.parent, L.L, u.parent)
+        $op(v)
+    end
+
+    @eval function LinearAlgebra.mul!(v::$VType, L::$LType, u::$VType, α::Number, β::Number)
+        mul!(v.parent, L.L, u.parent, α, β)
+        v
+    end
+
+    @eval function LinearAlgebra.ldiv!(v::$VType, L::$LType, u::$VType)
+        ldiv!(v.parent, L.L, u.parent)
+    end
+    
+    @eval function LinearAlgebra.ldiv!(L::$LType, u::$VType)
+        ldiv!(L.L, u.parent)
+        u
+    end
 end
 #
