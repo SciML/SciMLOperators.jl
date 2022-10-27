3ac31acaba7cbc82b6d9b30b93dc981f9b558c19
ArnoStrouwen <arno.strouwen@telenet.be>
2022-10-27 08:35:40 +0200

    clean up CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,10 @@
 using SafeTestsets
 
-const GROUP = get(ENV, "GROUP", "All")
-const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
-const is_TRAVIS = haskey(ENV, "TRAVIS")
-
 @time begin
-if GROUP == "All" || GROUP == "OperatorInterface"
     @time @safetestset "Scalar Operators" begin include("scalar.jl") end
     @time @safetestset "Basic Operators" begin include("basic.jl") end
     @time @safetestset "Matrix Operators" begin include("matrix.jl") end
     @time @safetestset "Function Operator" begin include("func.jl") end
     @time @safetestset "Full tests" begin include("total.jl") end
-
     @time @safetestset "Zygote.jl" begin include("zygote.jl") end
-end
 end
