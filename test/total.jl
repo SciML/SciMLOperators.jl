17298fb6b5a151b48a7bd0a472ffd5ecedbc2e95
Gaurav Arya <gauravarya272@gmail.com>
2023-03-12 13:02:30 -0400

    Test operator update (including kwarg update) in operator algebra test

--- test/total.jl
+++ test/total.jl
@@ -73,20 +73,31 @@ end
 
 @testset "Operator Algebra" begin
     N2 = N*N
+
     A = rand(N,N)
-    B = rand(N,N)
+    # Introduce update function for B
+    B = MatrixOperator(zeros(N,N); update_func=(A, u, p, t) -> (A .= p))
     C = rand(N,N)
-    D = rand(N,N)
+    # Introduce update function for D dependent on kwarg "matrix"
+    D = MatrixOperator(zeros(N,N); update_func=(A, u, p, t; matrix) -> (A .= p*t*matrix), 
+                       accepted_kwargs=(:matrix,))
 
     u = rand(N2,K)
+    p = rand()
+    t = rand()
+    matrix = rand(N, N)
+    diag = rand(N2)
     α = rand()
     β = rand()
 
     T1 = ⊗(A, B)
     T2 = ⊗(C, D)
 
-    D1  = DiagonalOperator(rand(N2))
-    D2  = DiagonalOperator(rand(N2))
+    # Introduce update function for D1
+    D1  = DiagonalOperator(p * ones(N2); update_func=(A, u, p, t) -> (A .= p))
+    # Introduce update funcion for D2 dependent on kwarg "diag" 
+    D2  = DiagonalOperator(p*t * diag; update_func=(A, u, p, t; diag) -> (A .= p*t*diag),
+                           accepted_kwargs=(:diag,))
 
     TT = [T1, T2]
     DD = Diagonal([D1, D2])
@@ -94,7 +105,19 @@ end
     op = TT' * DD * TT
     op = cache_operator(op, u)
 
+    # Update operator
+    @test_nowarn update_coefficients!(op, u, p, t; diag, matrix)
+    # Form dense operator manually 
+    dense_T1 = kron(A, p * ones(N, N))
+    dense_T2 = kron(C, (p*t) .* matrix)
+    dense_DD = Diagonal(vcat(p * ones(N2), p*t*diag))
+    dense_op = hcat(dense_T1', dense_T2') * dense_DD * vcat(dense_T1, dense_T2)
+    # Test correctness of op
+    @test op * u ≈ dense_op * u
+    @test convert(AbstractMatrix, op) ≈ dense_op 
+    # Test consistency with three-arg mul!
     v=rand(N2,K); @test mul!(v, op, u) ≈ op * u
+    # Test consistency with in-place five-arg mul!
     v=rand(N2,K); w=copy(v); @test mul!(v, op, u, α, β) ≈ α*(op * u) + β * w
 end
 #
