2c4043a86b97259fda8154359ae1a8f588af3a13
Vedant Puri <vedantpuri@gmail.com>
2023-05-10 18:32:02 -0400

    more comprehensive

--- test/zygote.jl
+++ test/zygote.jl
@@ -28,14 +28,22 @@ sca_update_func = (a, u, p, t) -> sum(p) * s
 vec_update_func = (b, u, p, t) -> Diagonal(p) * v
 mat_update_func = (A, u, p, t) -> Diagonal(p) * M
 inv_update_func = (A, u, p, t) -> inv(M) * inv(Diagonal(p))
+tsr_update_func = (A, u, p, t) -> reshape(p, n, n) |> copy
 
 α = ScalarOperator(zero(Float32), update_func = sca_update_func)
 L_dia = DiagonalOperator(zeros(N, K); update_func = vec_update_func)
 L_mat = MatrixOperator(zeros(N,N); update_func = mat_update_func)
 L_aff = AffineOperator(L_mat, L_mat, zeros(N, K); update_func = vec_update_func)
 L_sca = α * L_mat
+# TODO - fix InvertibleOperator constructor after merging
+# https://github.com/SciML/SciMLOperators.jl/pull/179
 L_inv = InvertibleOperator(MatrixOperator(M))
-L_fun = FunctionOperator((u,p,t) -> M * u, u0, u0; op_inverse=(u,p,t) -> M \ u)
+L_fun = FunctionOperator((u,p,t) -> Diagonal(p) * u, u0, u0;
+                         op_inverse=(u,p,t) -> Diagonal(p) \ u)
+
+Ti = MatrixOperator(zeros(n, n); update_func = tsr_update_func)
+To = deepcopy(Ti)
+L_tsr = TensorProductOperator(To, Ti)
 
 for (op_type, A) in
     (
@@ -49,7 +57,7 @@ for (op_type, A) in
      (BatchedDiagonalOperator, L_dia),
      (AddedOperator, L_mat + L_dia),
      (ComposedOperator, L_mat * L_dia),
-     (TensorProductOperator, TensorProductOperator(rand(n,n), rand(n,n))),
+     (TensorProductOperator, L_tsr),
      (FunctionOperator, L_fun),
 
      ## ignore wrappers
@@ -66,9 +74,7 @@ for (op_type, A) in
     loss_mul = function(p)
 
         v = Diagonal(p) * u0
-
         w = A(v, p, t)
-
         l = sum(w)
     end
 
