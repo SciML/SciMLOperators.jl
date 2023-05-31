b16ef2810dc14deaf5b85ce5385cad85f7def19a
Vedant Puri <vedantpuri@gmail.com>
2023-05-31 09:06:35 -0400

    no more warning

--- src/SciMLOperators.jl
+++ src/SciMLOperators.jl
@@ -21,7 +21,7 @@ import SparseArrays: sparse, issparse
 """
 $(TYPEDEF)
 
-## Interface
+# Interface
 
 An `L::AbstractSciMLOperator` can be called like a function. This behaves
 like multiplication by the linear operator represented by the
@@ -30,10 +30,19 @@ like multiplication by the linear operator represented by the
 - `L(du, u, p, t)` for in-place operator evaluation
 - `du = L(u, p, t)` for out-of-place operator evaluation
 
-If the operator is not a constant, update it with `(u, p, t)`.
-A mutating form, i.e. `update_coefficients!(L, u, p, t)` that changes the
-internal coefficients, and an out-of-place form
-`L_new = update_coefficients(L, u, p, t)`.
+Operator evaluation methods update its coefficients with `(u, p, t)`
+information using the `update_coefficients(!)` method. The methods
+are exported and can be called as follows:
+
+- `update_coefficients!(L, u, p, t)` for out-of-place operator update
+- `L = update_coefficients(L, u, p, t)` for in-place operator update
+
+SciMLOperators also overloads `Base.*`, `LinearAlgebra.mul!`,
+`LinearAlgebra.ldiv!` for operator evaluation without updating operator state.
+An `AbstractSciMLOperator` behaves like a matrix in these methods.
+Allocation-free methods, suffixed with a `!` often need cache arrays.
+To precache an `AbstractSciMLOperator`, call the function
+`L = cache_operator(L, input_vector)`.
 """
 abstract type AbstractSciMLOperator{T} end
 
