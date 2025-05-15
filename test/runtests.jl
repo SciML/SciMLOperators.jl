d38b165f203447ec2410e55b5a3d77534f2ac403
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-05-15 09:48:57 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,24 @@
-using SafeTestsets
+using SafeTestsets, Test
 
 @time begin
-    @time @safetestset "Scalar Operators" begin
-        include("scalar.jl")
-    end
-    @time @safetestset "Basic Operators" begin
-        include("basic.jl")
-    end
-    @time @safetestset "Matrix Operators" begin
-        include("matrix.jl")
-    end
-    @time @safetestset "Function Operator" begin
-        include("func.jl")
-    end
-    @time @safetestset "Full tests" begin
-        include("total.jl")
-    end
-    @time @safetestset "Zygote.jl" begin
-        include("zygote.jl")
+    @testset "SciMLOperators" begin
+        @time @safetestset "Scalar Operators" begin
+            include("scalar.jl")
+        end
+        @time @safetestset "Basic Operators" begin
+            include("basic.jl")
+        end
+        @time @safetestset "Matrix Operators" begin
+            include("matrix.jl")
+        end
+        @time @safetestset "Function Operator" begin
+            include("func.jl")
+        end
+        @time @safetestset "Full tests" begin
+            include("total.jl")
+        end
+        @time @safetestset "Zygote.jl" begin
+            include("zygote.jl")
+        end
     end
 end
